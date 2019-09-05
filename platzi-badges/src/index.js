76f446e2d883204ac0ac00cade9c9ec1483ca47e
Gabuvi7 <gabriel.uviedo@sarsy-srl.com>
2019-09-05 00:09:33 -0300

    Inicializo app

---
+++ platzi-badges/src/index.js
@@ -0,0 +1,20 @@
+// const element = document.createElement('h1');
+// element.innerText = 'Hello test';
+
+// const container = document.getElementById('app')
+
+// container.appendChild(element)
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hello, Platzi Badges from React!</h1>
+
+const container = document.getElementById('app');
+
+//ReactDOM.render(__que__,__donde__);
+
+ReactDOM.render(element,container);
+
+
+//Siempre que use jsx debo importar react en cada documento.
